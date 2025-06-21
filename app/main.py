59fbaf2301b20cba716c73f955788c8aeeecf0e1
Samuel Oliveira <patriciosamuel097@gmail.com>
2025-06-20 23:43:43 -0300

    streamlit app code

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 import streamlit as st
+from PIL import Image
 
-st.title('My first Streamlit App')
+st.title('Facial Emotion Recognition')
 
-st.text('Welcome to my Streamlit app')
+st.text('emotion detection using deep learning')
 
 user_input = st.text_input('Enter a custom messagem:', 'Hello, Streamlit')
 
 st.write('Customized Message:', user_input)
+
+uploaded_image = st.file_uploader("Choose an image file", type="jpg")
+
+if uploaded_image:
+    image = Image.open(uploaded_image)
+    image.save("./images/output_image.jpg")
+    st.image("./images/output_image.jpg")
